6a539038f3da5502e028248b377cbe25961c3811
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-05-11 23:13:25 -0300

    initial

---
+++ APICatalogo/APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        { }
+
+        // mapeia as entidades
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20200512011015_Inicial2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class Inicial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categorias",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categorias",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 300);
+        }
+    }
+}
