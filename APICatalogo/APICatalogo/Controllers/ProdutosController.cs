cabd87e4096319b0afb250c2123e05d0856293f5
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-05-12 08:55:57 -0300

    ProdutoController

---
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,57 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        //injeção de dependencia do serviço
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            // AsNoTracking() aumenta desempenho
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            //Desde a versão 2.1 essa validação ja é feita
+            // if (!ModelState.IsValid)
+            // {
+            //   return BadRequest(ModelState);
+            //  }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            
+            return new CreatedAtRouteResult("ObterProduto",new { id = produto.ProdutoId }, produto);
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20200512102543_Populadb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class Populadb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas','http://macoratti.net/Imagens/1.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches','http://macoratti.net/Imagens/2.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas','http://macoratti.net/Imagens/3.jpg')");
+
+
+            mb.Sql("Insert into Produtos(Nome, Descricao,Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Cocacola Diet'," +
+                "'Reefrigerante de Cola 350ml', 5.45, 'http://macoratti.net/Imagens/coca.jpg',50, now()," +
+                "(Select CategoriaId from Categorias where Nome='Bebidas'))");
+
+            mb.Sql("Insert into Produtos(Nome, Descricao,Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Lanche de Atum'," +
+                "'Lanche de Atum com maionese', 8.50, 'http://macoratti.net/Imagens/atum.jpg',10, now()," +
+                "(Select CategoriaId from Categorias where Nome='Lanches'))");
+
+            mb.Sql("Insert into Produtos(Nome, Descricao,Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Pudim 100g'," +
+                "'Pudim de Leite Condensado', 6.75, 'http://macoratti.net/Imagens/pudim.jpg',20, now()," +
+                "(Select CategoriaId from Categorias where Nome='Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
