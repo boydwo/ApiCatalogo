5e506fe5c8fc23cf247d2be401bb620aef6dac74
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-06-19 11:03:05 -0300

    test

---
+++ APICatalogo/APICatalogo/DTOs/UsuarioDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.DTOs
+{
+    public class UsuarioDTO
+    {
+        public string Email { get; set; }
+        public string Passoword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
